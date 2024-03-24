48b4b290db8407c71df88f4ebef3ed18334305e5
Julie B. <no-reply@bbjubjub.fr>
2024-03-24 12:51:57 +0100

    solve pow

--- solve.py
+++ solve.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-import os, json, time
+import os, json, time, string
 
 from eth_account import Account
 from web3 import Web3, HTTPProvider
@@ -12,6 +12,17 @@ r = remote('localhost', 1337)
 r.recvuntil(b'action? ')
 r.sendline(b'1')
 
+r.recvuntil("prefix:")
+prefix = r.recvline().strip().decode()
+r.recvuntil("difficulty:")
+difficulty = int(r.recvline().strip().decode())
+TARGET = 2**(256-difficulty)
+alphabet = string.ascii_letters+string.digits+'+/'
+answer = iters.bruteforce(lambda x: int.from_bytes(util.hashes.sha256sum((prefix + x).encode()), 'big') < TARGET, alphabet, length=7)
+r.sendlineafter(b">", answer)
+
+r.recvuntil(b'token:')
+token = r.recvline().strip()
 r.recvuntil(b'rpc endpoint:')
 rpc_url = r.recvline().strip().decode()
 r.recvuntil(b'private key:')
