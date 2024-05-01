7994224e34f27c694bc1182639ae6168f33dc7a8
Julie B. <no-reply@bbjubjub.fr>
2024-05-01 21:32:50 +0200

    actions

--- lib/base.py
+++ lib/base.py
@@ -9,19 +9,35 @@ from . import util
 
 
 @dataclasses.dataclass
+class Action:
+    description: str
+    handler: typing.Callable[[], typing.Awaitable[None]]
+
+
 class ChallengeBase(util.TextStreamRequestHandler, ABC):
     # per-subclass key value database to store challenge instance information
     metadata: dict[str, typing.Any]
 
-    def __init__(self, request, client_address, server) -> None:
-        super().__init__(request, client_address, server)
+    @property
+    def actions(self) -> list[Action]:
+        return [
+            Action("say hello", self._say_hello),
+        ]
 
-    async def ahandle(self) -> None:
+    async def _say_hello(self) -> None:
         self.print("hello web3")
 
     def handle(self) -> None:
+        for i, a in enumerate(self.actions):
+            self.print(f"{i} - {a.description}")
+        choice = self.input("> ")
+        try:
+            handler = self.actions[int(choice)].handler
+        except:
+            self.print("ngmi")
+            return
         loop = asyncio.new_event_loop()
-        loop.run_until_complete(self.ahandle())
+        loop.run_until_complete(handler)
 
     @classmethod
     def make_handler_class(cls) -> type[socketserver.BaseRequestHandler]:
